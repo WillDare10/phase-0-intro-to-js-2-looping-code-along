c6431dcba699c0ecf579987d1b4c3a31b99f9f05
William Dare <williamdare20@gmail.com>
2023-02-14 09:55:08 -0800

    small changes

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+const writeCards = ["Charlie", "Samip","Ali"];
+
+function writeThankyou(birthday){
+    let i = 0;
+    while (i < birthday.length){
+        console.log (`Thank you, ${writeCards[0]}for the wonderful birthday gift!`);
+    
+    }
+    return writeCards;
+}
+writeThankyou(writeCards);
